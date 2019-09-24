f9f58e35d2edd934d4788aced773e5b10fb7d2e1
Mark West <mark@telemething.com>
2019-09-24 16:07:04 -0700

    Trivial ROS add

---
+++ include/mfuse_ros.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+// ROS
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/String.h>
+#include <image_transport/image_transport.h>
+#include <iostream>
+
+//#include "spdlog/spdlog.h"
+//#include "spdlog/sinks/stdout_color_sinks.h"
+//#include "spdlog/sinks/basic_file_sink.h"
+
+//#include "livox_sdk.h"
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+
+
---
+++ src/mfuse_ros_node.cpp
@@ -0,0 +1,24 @@
+/*
+ * livox_to_octree_node.cpp
+ *
+ *  Created on: 1.1.1
+ *      Author: Mark West
+ *   
+ */
+
+#include <mfuse_ros.hpp>
+
+int main(int argc, char** argv) {
+
+  /*if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info)) 
+  {
+    ros::console::notifyLoggerLevelsChanged();
+  }
+
+  ros::init(argc, argv, "livox_to_octree");
+  ros::NodeHandle nodeHandle("~");
+  l2o::livox_to_octree livoxToOctree(nodeHandle);*/
+
+  ros::spin();
+  return 0;
+}
